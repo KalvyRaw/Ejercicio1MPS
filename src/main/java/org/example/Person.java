0efd3b850983c60edbbd9836a6b9a38fbad8141e
FerRaw <fernandouni@uma.es>
2023-02-12 18:43:16 +0100

    Segunda comprobación test, falla 1 test todavía.

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Person {
-    private final String nombre;
+    private final String name;
     private final int age;
 
     private final String gender;
 
     public Person(String nombre, int age, String gender) {
-        this.nombre = nombre;
+        this.name = nombre;
         this.age = age;
         this.gender = gender;
     }
 
     public String name(){
-        return nombre;
+        return name;
     }
 
     public int age(){
@@ -32,22 +32,24 @@ public class Person {
         List<Integer> fem = new ArrayList<>();
         int avgMale=0;
         int avgFem=0;
-        double[] res = new double[2];
-        for(int i=0; i<persons.size(); i++){
-            if(persons.get(i).gender.equalsIgnoreCase("Male")) {
-                masc.add(persons.get(i).age());
-            } else {
-                fem.add(persons.get(i).age());
+        double[] res = new double[]{0.0, 0.0};
+        if (!persons.isEmpty()){
+            for(int i=0; i<persons.size(); i++){
+                if(persons.get(i).gender.equalsIgnoreCase("Male")) {
+                    masc.add(persons.get(i).age());
+                } else {
+                    fem.add(persons.get(i).age());
+                }
             }
+            for(int i=0; i<masc.size();i++){
+                avgMale+= masc.get(i);
+            }
+            for(int i=0; i<fem.size();i++){
+                avgFem+= fem.get(i);
+            }
+            res[0] = avgMale/masc.size();
+            res[1] = avgFem/fem.size();
         }
-        for(int i=0; i<masc.size();i++){
-            avgMale+= masc.get(i);
-        }
-        for(int i=0; i<fem.size();i++){
-            avgFem+= fem.get(i);
-        }
-        res[0] = avgMale/masc.size();
-        res[1] = avgFem/fem.size();
         return res;
     }
 }
